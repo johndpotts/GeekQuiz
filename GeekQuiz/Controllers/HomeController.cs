a952a0c4e69cf139d3eee4e04cdabe1aa5073aec
John Potts <john@arlingtonchurch.com>
2017-10-11 06:49:42 -0400

    add contact email

--- GeekQuiz/Controllers/HomeController.cs
+++ GeekQuiz/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using GeekQuiz.Models;
 
 namespace GeekQuiz.Controllers
 {
@@ -27,6 +31,43 @@ namespace GeekQuiz.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Sent()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(EmailFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("jdpotts189@gmail.com"));  
+                message.From = new MailAddress("emailclient212327@gmail.com"); 
+                message.Subject = "Contact Request";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "emailclient212327@gmail.com", 
+                        Password = "@ma$on!!" 
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
         }
     }
 }
--- GeekQuiz/Scripts/site.js
+++ GeekQuiz/Scripts/site.js
@@ -4,7 +4,7 @@ $('.section-scroll').on('click', function (e) {
     var $target = $(target);
 
     $('html, body').stop().animate({
-        'scrollTop': $target.offset().top
+        'scrollTop': $target.offset().top -80
     }, 900, 'swing');
 
     e.preventDefault();
